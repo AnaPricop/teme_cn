b5706bdfe24bcd47e4fde5856df7ed5f03a17348
AnaPricop <60388013+AnaPricop@users.noreply.github.com>
2022-05-16 13:55:35 +0300

    Update main.py

--- tema4/main.py
+++ tema4/main.py
@@ -19,7 +19,7 @@ def read_files(file_a, file_b):
             i = int(line_sp[1])
             j = int(line_sp[2])
             if validare_i_j((i, j)):
-                if if_exists_i_j((i, j), a): # daca gasim aceeasi indexi, adaugam la val initiala
+                if if_exists_i_j((i, j), a):  # daca gasim aceeasi indexi, adaugam la val initiala
                     i2 = i
                     j2 = j
                     line = a[i2]
@@ -29,7 +29,7 @@ def read_files(file_a, file_b):
                 else:
                     entry = (value, j)
                     a[i].append(entry)
-            if i == j: #daca gasim element de pe diagonala
+            if i == j:  # daca gasim element de pe diagonala
                 diag.append(value)
     with open(file_b) as f:
         b = []
@@ -81,7 +81,6 @@ mat_b4, mat_a4, d4, len4 = read_files('a_4.txt', 'b_4.txt')
 
 mat_b5, mat_a5, d5, len5 = read_files('a_5.txt', 'b_5.txt')
 
-
 if not verificare_diagonala_a(mat_a1, len1):
     print('Am gasit element nul pe diagonala matricei a, deci nu putem aplica Jacobi.')
 if not verificare_diagonala_a(mat_a2, len2):
@@ -113,34 +112,33 @@ def verify_solution(a, b, x):
 
 
 def jacobi_method(a, b, d, len):
-    x_current = [0 for _ in range(len)]
-    x_previous = [0 for _ in range(len)]
+    x_c = [0 for _ in range(len)] # pseudocod
+    x_p = [0 for _ in range(len)]
     k = 0
     delta_x = eps
     k_max = 10000
     while delta_x >= eps and k <= k_max and delta_x <= 1e+8:
-        x_previous = x_current
-        x_current = step_formula(x_previous, a, b, d, len)
-        delta_x = np.linalg.norm(np.subtract(x_current, x_previous))
+        x_p = x_c
+        x_c = jacobi_st(x_p, a, b, d, len)
+        delta_x = np.linalg.norm(np.subtract(x_c, x_p))
         k = k + 1
     if delta_x < eps:
         print('Iterations:', k)
-        return x_current
+        return x_c
     if not verificare_diagonala_a(a, len):
         return None
     return None
 
 
-def step_formula(x_prev, a, b, d, size):
+def jacobi_st(x_prev, a, b, d, size):
     x = [b[i] for i in range(size)]
     for i in range(size):
         for element in a[i]:
             value = element[0]
             j = element[1]
             if i != j:
-                x[i] -= value * x_prev[j]  # sum1
-                x[j] -= value * x_prev[i]  # sum2
-
+                x[i] -= value * x_prev[j]
+                x[j] -= value * x_prev[i]
     for i in range(size):
         x[i] /= d[i]
 
@@ -174,10 +172,10 @@ if xj4 is None:
 else:
     norma4 = verify_solution(mat_a4, mat_b4, xj4)
     print('Norma 4:', norma4)
-
-xj5 = jacobi_method(mat_a5, mat_b5, d5, len5)
-if xj5 is None:
-    print('divergence')
-else:
-    norma5 = verify_solution(mat_a5, mat_b5, xj5)
-    print('Norma 5:', norma5)
+#
+# xj5 = jacobi_method(mat_a5, mat_b5, d5, len5)
+# if xj5 is None:
+#     print('divergence')
+# else:
+#     norma5 = verify_solution(mat_a5, mat_b5, xj5)
+#     print('Norma 5:', norma5)
